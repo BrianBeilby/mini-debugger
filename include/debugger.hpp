3b86e3759622bd6b564b7786a26299f2dadc9de9
BrianBeilby <brianbeilby98@gmail.com>
2024-07-12 12:35:27 -0700

    Update debugger.hpp

--- include/debugger.hpp
+++ include/debugger.hpp
@@ -1,6 +1,12 @@
-#include <string>
+#ifndef MINIDBG_DEBUGGER_HPP
+#define MINIDBG_DEBUGGER_HPP
 
-class debugger {
+#include <utility>
+#include <string>
+#include <linux/types.h>
+
+namespace minidbg {
+    class debugger {
     public:
         debugger (std::string prog_name, pid_t pid)
             : m_prog_name{std::move(prog_name)}, m_pid{pid} {}
@@ -8,6 +14,12 @@ class debugger {
         void run();
 
     private:
+        void handle_command(const std::string& line);
+        void continue_execution();        
+        
         std::string m_prog_name;
         pid_t m_pid;
-};
+    };
+}
+
+#endif
