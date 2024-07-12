2a6eed9ac7ef439ccb7b37750ae1eac008eac8da
BrianBeilby <brianbeilby98@gmail.com>
2024-07-12 12:35:24 -0700

    Update minidbg.cpp

--- src/minidbg.cpp
+++ src/minidbg.cpp
@@ -1,10 +1,15 @@
-#include <iostream>
-#include <unistd.h>
+#include <vector>
 #include <sys/ptrace.h>
 #include <sys/wait.h>
+#include <unistd.h>
+#include <sstream>
+#include <iostream>
 
 #include "linenoise.h"
+
 #include "debugger.hpp"
+
+using namespace minidbg;
 
 void debugger::run() {
     int wait_status;
@@ -16,6 +21,34 @@ void debugger::run() {
         handle_command(line);
         linenoiseHistoryAdd(line);
         linenoiseFree(line);
+    }
+}
+
+std::vector<std::string> split(const std::string &s, char delimiter) {
+    std::vector<std::string> out{};
+    std::stringstream ss {s};
+    std::string item;
+
+    while (std::getline(ss, item, delimiter)) {
+        out.push_back(item);
+    }
+
+    return out;
+}
+
+bool is_prefix(const std::string& s, const std::string& of) {
+    if (s.size() > of.size()) return false;
+    return std::equal(s.begin(), s.end(), of.begin());
+}
+
+void debugger::handle_command(const std::string& line) {
+    auto args = split(line, ' ');
+    auto command = args[0];
+
+    if (is_prefix(command, "continue")) {
+        continue_execution();
+    } else {
+        std::cerr << "Unknown command\n";
     }
 }
 
