4b338f2638f236c77ae5584769e16dd4156b4f4c
BrianBeilby <brianbeilby98@gmail.com>
2024-07-17 13:13:48 -0700

    Update debugger.hpp

--- include/debugger.hpp
+++ include/debugger.hpp
@@ -26,6 +26,8 @@ namespace minidbg {
         void set_breakpoint_at_address(std::intptr_t addr);
         void dump_registers();
         void print_source(const std::string& file_name, unsigned line, unsigned n_lines_context=2);
+        void single_step_instruction();
+        void single_step_instruction_with_breakpoint_check();
 
     private:
         void handle_command(const std::string& line);
