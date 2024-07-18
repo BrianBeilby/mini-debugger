cfeafca93de0fbc4fc759b2eae254bdd8fef091c
BrianBeilby <brianbeilby98@gmail.com>
2024-07-18 11:00:45 -0700

    Update debugger.hpp

--- include/debugger.hpp
+++ include/debugger.hpp
@@ -57,6 +57,7 @@ namespace minidbg {
         void step_over();
         void step_out();
         void remove_breakpoint(std::intptr_t addr);
+        auto lookup_symbol(const std::string& name) -> std::vector<symbol>;
 
     private:
         void handle_command(const std::string& line);
