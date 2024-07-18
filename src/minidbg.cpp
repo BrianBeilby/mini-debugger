caefe5228b8bfd659eaca48816e637ea2158adef
BrianBeilby <brianbeilby98@gmail.com>
2024-07-18 10:51:55 -0700

    Update minidbg.cpp

--- src/minidbg.cpp
+++ src/minidbg.cpp
@@ -20,6 +20,17 @@
 
 using namespace minidbg;
 
+symbol_type to_symbol_type(elf::stt sym) {
+    switch (sym) {
+    case elf::stt::notype: return symbol_type::notype;
+    case elf::stt::object: return symbol_type::object;
+    case elf::stt::func: return symbol_type::func;
+    case elf::stt::section: return symbol_type::section;
+    case elf::stt::file: return symbol_type::file;
+    default: return symbol_type::notype;
+    }
+};
+
 void debugger::initialise_load_address() {
    //If this is a dynamic library (e.g. PIE)
    if (m_elf.get_hdr().type == elf::et::dyn) {
