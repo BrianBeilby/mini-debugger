5e7d2434541bf4b0ae5a2066dd6302284bd4e55e
BrianBeilby <brianbeilby98@gmail.com>
2024-07-20 09:21:16 -0700

    Update minidbg.cpp

--- src/minidbg.cpp
+++ src/minidbg.cpp
@@ -176,6 +176,41 @@ void debugger::single_step_instruction_with_breakpoint_check() {
     }
 }
 
+void debugger::read_variables() {
+    using namespace dwarf;
+
+    auto func = get_function_from_pc(get_offset_pc());
+
+    for (const auto& die : func) {
+        if (die.tag == DW_TAG::variable) {
+            auto loc_val = die[DW_AT::location];
+            if (loc_val.get_type() == value::type::exprloc) {
+                ptrace_expr_context context {m_pid};
+                auto result = loc_val.as_exprloc().evaluate(&context);
+                switch (result.location_type) {
+                    case expr_result::type::address:
+                    {
+                        auto value = read_memory(result.value);
+                        std::cout << at_name(die) << "(0x" << std::hex << result.value << ") = "
+                                  << value << std::endl;
+                    }
+
+                    case expr_result::type::reg:
+                    {
+                        auto value = get_register_value_from_dwarf_register(m_pid, result.value);
+                        std::cout << at_name(die) << " (reg " << result.value << ") = "
+                                  << value << std::endl;
+                        break;
+                    }
+
+                    default:
+                        throw std::runtime_error{"Unhandled variable location"};
+                }
+            }
+        }
+    }
+}
+
 uint64_t debugger::read_memory(uint64_t address) {
     return ptrace(PTRACE_PEEKDATA, m_pid, address, nullptr);
 }
@@ -426,6 +461,9 @@ void debugger::handle_command(const std::string& line) {
     }
     else if (is_prefix(command, "backtrace")) {
         print_backtrace();
+    }
+    else if (is_prefix(command, "variables")) {
+        read_variables();
     }
     else {
         std::cerr << "Unknown command\n";
